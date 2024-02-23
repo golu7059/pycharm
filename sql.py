f4cc5f99d88932cb884980dafa22b9a8575eb8d4
golu7059 <golukumarjaisidih@gmail.com>
2024-02-23 09:17:14 +0530

    sql connected

---
+++ sql.py
@@ -0,0 +1,3 @@
+import mysql.connector as con
+db = con.connect(host="localhost", user="root" , password="Golu@1234")
+print("connected to mysql")
---
+++ tinker.py
@@ -0,0 +1,5 @@
+from tkinter import *
+a=Tk()
+a.title("first GUI application")
+a.geometry("400x400")
+a.mainloop()
